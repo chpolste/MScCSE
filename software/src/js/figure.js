0442a7994f9a58f28f75261669a1fd8f8557057b
Christopher Polster <chpolste@tuta.io>
2018-05-27 17:50:39 +0200

    Add plotting of halfspaces
    Also fixes a bug with out-of-bounds ticks in linearTicks

--- software/src/js/figure.js
+++ software/src/js/figure.js
@@ -3,13 +3,16 @@
 
 import type { ElementAttributes, ElementEvents } from "./domtools.js";
 import type { Observer, Observable } from "./tools.js";
-import { ObservableMixin } from "./tools.js";
+
+import * as linalg from "./linalg.js";
+import { ObservableMixin, xor } from "./tools.js";
 
 
 type ShapeExt = { style?: ElementAttributes, events?: ElementEvents };
 export type Shape = ({ kind: "polytope", vertices: Point[] } & ShapeExt)
                   | ({ kind: "arrow", origin: Point, target: Point } & ShapeExt)
-                  | ({ kind: "text", coords: Point, text: string} & ShapeExt)
+                  | ({ kind: "text", coords: Point, text: string } & ShapeExt)
+                  | ({ kind: "halfspace", normal: Point, offset: number } & ShapeExt)
                   | ({ kind: "vectorField", fun: Point => Point, n?: number[] } & ShapeExt);
 
 export type Primitive = { kind: "polygon", points: Point[] }
@@ -85,8 +88,11 @@ type Range = [number, number];
 type Tick = [number, string];
 
 export interface Projection {
+    // Project to normalized [0, 1] x [0, 1] coordinate system
     fwd(p: Point): Point;
+    // Inverse projection (if possible)
     bwd(p: Point): Point;
+    // Translate a shape to plottable primitives
     project(shape: Shape): Primitive[];
     zoom(factor: number): Projection;
     translate(start: Point, end: Point): Projection;
@@ -107,14 +113,16 @@ function linearTicks(min: number, max: number, n: number): number[] {
     if (min <= 0 && 0 <= max) {
         init = 0;
     } else if (max - min > lp10 * 10) {
-        init = Math.ceil(min / 10) * 10;
+        init = Math.ceil(min / lp10 / 10) * lp10 * 10;
     } else {
         init = Math.ceil(min / lp10) * lp10;
     }
     const ticks = [];
-    for (let i = init; i >= min; i = i - increment) ticks.push(i);
-    for (let i = init + increment; i <= max; i = i + increment) ticks.push(i);
-    return ticks.sort();
+    for (let i = -n; i <= n; i++) {
+        const tick = init + i * increment;
+        if (min <= tick && tick <= max) ticks.push(tick);
+    }
+    return ticks;
 }
 
 
@@ -151,19 +159,49 @@ export class Cartesian2D implements Projection {
 
     project(shape: Shape): Primitive[] {
         let primitives = [];
+        // 2-dimensional polytopes are polygons which are a primitive
         if (shape.kind === "polytope") {
             primitives.push({
                 kind: "polygon",
                 points: shape.vertices.map(vertex => this.fwd(vertex))
             });
+        // Arrows are a primitive shape, project origin and target coordinates
         } else if (shape.kind === "arrow") {
             primitives.push({
                 kind: "arrow",
                 origin: this.fwd(shape.origin),
                 target: this.fwd(shape.target)
             });
+        // Text containers are a primitive
         } else if (shape.kind === "text") {
             primitives.push({ kind: "text", coords: this.fwd(shape.coords), text: shape.text });
+        // Halfspaces are transformed into a polygon that covers the visible
+        // part of the projection up to the halfspace edge
+        } else if (shape.kind === "halfspace") {
+            const normal = shape.normal;
+            const offset = shape.offset;
+            const cs = [
+                [this.minX, this.minY], [this.minX, this.maxY],
+                [this.maxX, this.maxY], [this.maxX, this.minY]
+            ];
+            const csIn = cs.map(v => linalg.dot(v, normal) - offset < linalg.TOL);
+            // Move around rectangle and add corners and, if appropriate,
+            // intersections of the halfplane-edge with the edges of the
+            // rectangle
+            const vertices = [];
+            if (csIn[0]) vertices.push(cs[0]);
+            if (xor(csIn[0], csIn[1])) vertices.push([cs[0][0], (offset - normal[0] * cs[0][0]) / normal[1]]);
+            if (csIn[1]) vertices.push(cs[1]);
+            if (xor(csIn[1], csIn[2])) vertices.push([(offset - normal[1] * cs[1][1]) / normal[0], cs[1][1]]);
+            if (csIn[2]) vertices.push(cs[2]);
+            if (xor(csIn[2], csIn[3])) vertices.push([cs[2][0], (offset - normal[0] * cs[2][0]) / normal[1]]);
+            if (csIn[3]) vertices.push(cs[3]);
+            if (xor(csIn[3], csIn[0])) vertices.push([(offset - normal[1] * cs[3][1]) / normal[0], cs[3][1]]);
+            primitives.push({
+                kind: "polygon",
+                points: vertices.map(v => this.fwd(v))
+            });
+        // Vector fields are displayed as arrows and sampled using linearTicks
         } else if (shape.kind === "vectorField") {
             for (let x of linearTicks(this.minX, this.maxX, shape.n == null ? 10 : shape.n[0])) {
                 for (let y of linearTicks(this.minY, this.maxY, shape.n == null ? 10 : shape.n[1])) {
@@ -233,6 +271,8 @@ export class Horizontal1D implements Projection {
 
     project(shape: Shape): Primitive[] {
         let primitives = [];
+        // 1-dimensional polytopes are intervals, display with some height so
+        // they are better visible and can be filled with color
         if (shape.kind === "polytope") {
             if (shape.vertices.length < 2) {
                 return [];
@@ -242,10 +282,29 @@ export class Horizontal1D implements Projection {
                 kind: "polygon",
                 points: [[l[0], 0.6], [l[0], 0.4], [r[0], 0.4], [r[0], 0.6]]
             });
+        // Arrows are a primitive
         } else if (shape.kind === "arrow") {
             primitives.push({ kind: "arrow", origin: this.fwd(shape.origin), target: this.fwd(shape.target) });
+        // Text is a primitive
         } else if (shape.kind === "text") {
             primitives.push({ kind: "text", coords: [shape.coords[0], 0.5], text: shape.text });
+        } else if (shape.kind === "halfspace") {
+            const normal = shape.normal;
+            const offset = shape.offset;
+            let left = 0;
+            let right = 1;
+            if (normal[0] < 0) {
+                left = Math.max(left, this.fwd([-offset])[0]);
+            } else {
+                right = Math.min(right, this.fwd([offset])[0]);
+            }
+            if (left < right) {
+                primitives.push({
+                    kind: "polygon",
+                    points: [[left, 0], [left, 1], [right, 1], [right, 0]]
+                });
+            }
+        // Vector fields are displayed with arrows
         } else if (shape.kind === "vectorField") {
             for (let x of linearTicks(this.min, this.max, shape.n == null ? 10 : shape.n[0])) {
                 primitives.push({

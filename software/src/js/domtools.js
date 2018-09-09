chpolste/MScCSE

commit 19bc70b6e5da44578d7d0aca4d4b7516dab679c9

Indent continuations of overflowing action lines

software/src/css/inspector.css
@@ -358,6 +358,8 @@ span.nonsatisfying {
 }
 .action-view > div {
     display: block;
+    text-indent: -12pt;
+    margin-left: 12pt;
 }
 .action-view > div:hover {
     cursor: pointer;

commit 7a6e5284bb71fe3fa5f0d1434ca769e05f7dc8f0

Style state and predicate labels with subscripts
- Extend texifyProposition with symbolTransform argument
- Replace text with specialized label primitive
- Apply subscript styling to inspector
- Undo commit 19bc70b, which caused problem with hover background-color

software/src/css/inspector.css
@@ -138,8 +138,10 @@ button {
     display: inline-block;
 }
 
+/* https://stackoverflow.com/questions/1530685 */
 sub {
     font-size: 7pt;
+    vertical-align: bottom;
 }
 
 code {
@@ -368,8 +370,6 @@ span.nonsatisfying {
 }
 .action-view > div {
     display: block;
-    text-indent: -12pt;
-    margin-left: 12pt;
 }
 .action-view > div:hover {
     cursor: pointer;

software/src/js/domtools.js
@@ -138,6 +138,38 @@ export class Keybindings {
 }
 
 
+/* Label styling */
+
+const SPLIT_LABEL_REGEX = /^([a-zA-Z]+)(\d+)$/;
+const NUM_TSPAN_ATTRS = { "dy": "2", "font-size": "0.8em" };
+export const label = {
+    
+    // Split labels like X12 or p3 into ["X", "12"] or ["p", "13"]
+    split: function (text: string): [string, string] {
+        const match = SPLIT_LABEL_REGEX.exec(text);
+        return (match == null) ? [text, ""] : [match[1], match[2]];
+    },
+
+    toTeX: function (text: string): string {
+        const [name, num] = label.split(text);
+        return (num.length === 0) ? name : name + "_" + num;
+    },
+
+    toHTML: function (text: string): HTMLSpanElement {
+        const [name, num] = label.split(text);
+        return span({}, (num.length === 0) ? [name] : [name, create("sub", {}, [num])]);
+    },
+
+    toSVG: function (text: string): Element {
+        const [name, num] = label.split(text);
+        return createSVG("text", {},
+            (num.length === 0) ? [name] : [name, createSVG("tspan", NUM_TSPAN_ATTRS, [num])]
+        );
+    }
+
+};
+
+
 /* Inline Information display
 
 A (?)-button that reveals an infobox specified elsewhere (identified by id)

software/src/js/figure.js
@@ -12,14 +12,14 @@ type ShapeExt = { style?: ElementAttributes, events?: ElementEvents };
 export type Shape = ({ kind: "polytope", vertices: Point[] } & ShapeExt)
                   | ({ kind: "arrow", origin: Point, target: Point } & ShapeExt)
                   | ({ kind: "marker", coords: Point, size: number } & ShapeExt)
-                  | ({ kind: "text", coords: Point, text: string } & ShapeExt)
+                  | ({ kind: "label", coords: Point, text: string } & ShapeExt)
                   | ({ kind: "halfspace", normal: Point, offset: number } & ShapeExt)
                   | ({ kind: "vectorField", fun: Point => Point, n?: number[] } & ShapeExt);
 
 export type Primitive = { kind: "polygon", points: Point[] }
                       | { kind: "arrow", origin: Point, target: Point }
                       | { kind: "marker", coords: Point, size: number }
-                      | { kind: "text", coords: Point, text: string };
+                      | { kind: "label", coords: Point, text: string };
 
 
 
@@ -167,15 +167,15 @@ export class Cartesian2D implements Projection {
                 kind: "polygon",
                 points: shape.vertices.map(vertex => this.fwd(vertex))
             });
-        // arrow, text and marker are primitives
+        // arrow, label and marker are primitives
         } else if (shape.kind === "arrow") {
             primitives.push({
                 kind: "arrow",
                 origin: this.fwd(shape.origin),
                 target: this.fwd(shape.target)
             });
-        } else if (shape.kind === "text") {
-            primitives.push({ kind: "text", coords: this.fwd(shape.coords), text: shape.text });
+        } else if (shape.kind === "label") {
+            primitives.push({ kind: "label", coords: this.fwd(shape.coords), text: shape.text });
         } else if (shape.kind === "marker") {
             primitives.push({ kind: "marker", coords: this.fwd(shape.coords), size: shape.size });
         // Halfspaces are transformed into a polygon that covers the visible
@@ -288,11 +288,11 @@ export class Horizontal1D implements Projection {
                 kind: "polygon",
                 points: [[l[0], this.maxY], [l[0], this.minY], [r[0], this.minY], [r[0], this.maxY]]
             });
-        // arrow, text and marker are primitives
+        // arrow, label and marker are primitives
         } else if (shape.kind === "arrow") {
             primitives.push({ kind: "arrow", origin: this.fwd(shape.origin), target: this.fwd(shape.target) });
-        } else if (shape.kind === "text") {
-            primitives.push({ kind: "text", coords: this.fwd(shape.coords), text: shape.text });
+        } else if (shape.kind === "label") {
+            primitives.push({ kind: "label", coords: this.fwd(shape.coords), text: shape.text });
         } else if (shape.kind === "marker") {
             primitives.push({ kind: "marker", coords: this.fwd(shape.coords), size: shape.size });
         } else if (shape.kind === "halfspace") {

software/src/js/inspector-widgets-inspector.js
@@ -12,7 +12,8 @@ import type { Input } from "./widgets-input.js";
 
 import * as linalg from "./linalg.js";
 import * as dom from "./domtools.js";
-import { iter, arr, sets, n2s, t2s, replaceAll, ObservableMixin, WorkerCommunicator } from "./tools.js";
+import { iter, arr, sets, n2s, t2s, replaceAll, ObservableMixin,
+         WorkerCommunicator } from "./tools.js";
 import { union } from "./geometry.js";
 import { Objective, stringifyProposition, texifyProposition } from "./logic.js";
 import { AbstractedLSS, partitionMap, controller, refinery } from "./system.js";
@@ -74,10 +75,10 @@ function styledStateLabel(state: State, markSelected?: ?State): HTMLSpanElement 
     } else if (state.isNonSatisfying) {
         attributes["class"] = "nonsatisfying";
     }
-    return dom.span(attributes, [state.label]);
+    return dom.span(attributes, [dom.label.toHTML(state.label)]);
 }
 
-function asInequation(h: Halfspace): string {
+function toInequation(h: Halfspace): string {
     const terms = [];
     for (let i = 0; i < h.dim; i++) {
         if (h.normal[i] === 0) {
@@ -96,7 +97,9 @@ function asInequation(h: Halfspace): string {
 }
 
 function styledPredicateLabel(label: string, system: AbstractedLSS): HTMLSpanElement {
-    return dom.span({ "title": asInequation(system.getPredicate(label)) }, [label]);
+    const node = dom.label.toHTML(label);
+    node.setAttribute("title", toInequation(system.getPredicate(label)));
+    return node;
 }
 
 function matrixToTeX(m: Matrix): string {
@@ -162,7 +165,7 @@ export class ProblemSummary {
 
         let formula = objective.kind.formula;
         for (let [symbol, prop] of objective.propositions) {
-            formula = replaceAll(formula, symbol, "(" + texifyProposition(prop) + ")");
+            formula = replaceAll(formula, symbol, "(" + texifyProposition(prop, dom.label.toTeX) + ")");
         }
 
         this.node = dom.div({ "class": "problem-summary" }, [
@@ -174,13 +177,14 @@ export class ProblemSummary {
                 dom.div({}, [
                     dom.h3({}, ["Labeled Predicates"]),
                     ...Array.from(system.predicates.entries()).map(
-                        ([label, halfspace]) => dom.renderTeX(label + ": " + asInequation(halfspace), dom.p())
+                        ([label, halfspace]) => dom.renderTeX(dom.label.toTeX(label) + ": " + toInequation(halfspace), dom.p())
                     )
                 ])
             ]),
             dom.div({}, [
                 dom.h3({}, ["Objective"]),
-                dom.p({}, [objective.kind.name, ": ", dom.renderTeX(formula, dom.span())]) // TODO: use KaTeX
+                dom.renderTeX(formula, dom.p()),
+                dom.p({}, [objective.kind.name])
             ])
         ]);
     }
@@ -1130,7 +1134,7 @@ class SystemView {
         let labels = [];
         if (this.settings.toggleLabel.value) {
             labels = iter.map(state => ({
-                kind: "text", coords: state.polytope.centroid, text: state.label, style: {dy: "3"}
+                kind: "label", coords: state.polytope.centroid, text: state.label, style: {dy: "3"}
             }), this.system.states.values());
         }
         this.layers.label.shapes = labels;

software/src/js/logic.js
@@ -27,7 +27,7 @@ export class Objective {
     constructor(kind: ObjectiveKind, terms: Proposition[] ): void {
         this.kind = kind;
         if (terms.length !== kind.variables.length) throw new Error(
-            "" // TODO
+            "Number of terms (" + terms.length + ") does not match number of variables (" + kind.variables.length + ")"
         );
         this.propositions = new Map(arr.zip2(kind.variables, terms));
         this.automaton = OnePairStreettAutomaton.parse(kind.automaton);
@@ -46,6 +46,7 @@ export class AtomicProposition {
     +symbol: string;
 
     constructor(symbol: string): void {
+        // TODO: verify symbols
         this.symbol = symbol;
     }
 
@@ -129,9 +130,14 @@ function getOpOf(prop: Proposition): * {
             return op;
         }
     }
-    throw new Error(); // TODO
+    throw new Error("unknown operator");
 }
 
+// Accepted tokens are:
+// - Parentheses
+// - Operators !, &, |, ->
+// - Atomic proposition labels (start with small letter, then any number of
+//   numbers, letters and underscores)
 const parsePropositionAST = ASTParser(/[()!&|]|->|(?:[a-z][a-z0-9]*)/, PROP_OPS);
 
 function asProposition(node: ASTNode): Proposition {
@@ -143,7 +149,7 @@ function asProposition(node: ASTNode): Proposition {
                 return new op.cls(...node.args.map(asProposition));
             }
         }
-        throw new ParseError(); // TODO
+        throw new ParseError("unknown operator '" + node.op + "'");
     }
 }
 
@@ -162,9 +168,12 @@ export function stringifyProposition(prop: Proposition): string {
 }
 
 // TeX representation of propositional formula
-export function texifyProposition(prop: Proposition, parentOp?: *, rightArg?: boolean): string {
+export function texifyProposition(prop: Proposition, symbolTransform?: (string) => string, parentOp?: *, rightArg?: boolean): string {
+    if (symbolTransform == null) {
+        symbolTransform = _ => _; // Identity
+    }
     if (prop instanceof AtomicProposition) {
-        return prop.symbol;
+        return symbolTransform(prop.symbol);
     }
     const op = getOpOf(prop);
     let out = "";
@@ -176,17 +185,21 @@ export function texifyProposition(prop: Proposition, parentOp?: *, rightArg?: bo
         if (arg instanceof AtomicProposition) {
             out = op.tex + " " + arg.symbol;
         } else {
-            out = op.tex + " (" + texifyProposition(arg) + ")";
+            out = op.tex + " (" + texifyProposition(arg, symbolTransform) + ")";
         }
     // Binary operator: if parentheses are required depends on the operator
     // precedence when compared to the parent operator or if both have equal
     // precedence, on the argument position and associativity of the operator
     } else {
-        const left = texifyProposition(prop.args[0], op, false);
-        const right = texifyProposition(prop.args[1], op, true);
+        const left = texifyProposition(prop.args[0], symbolTransform, op, false);
+        const right = texifyProposition(prop.args[1], symbolTransform, op, true);
         out = left + " " + op.tex + " " + right;
+        // No parentheses for top-level expressions
         needsParentheses = parentOp != null && (
+            // If parent has higher precedence, parentheses are always needed
             parentOp.precedence > op.precedence || (
+                // If parent has same precedence, associativity and position
+                // decide over need for parentheses
                 parentOp.precedence === op.precedence && (
                     (rightArg && op.associativity < 0) || (!rightArg && op.associativity > 0)
                 )

software/src/js/widgets-plot.js
@@ -347,11 +347,11 @@ class ShapeGroup {
                     dom.addEventListeners(node, events);
                     children.push(node);
 
-                } else if (primitive.kind === "text") {
+                } else if (primitive.kind === "label") {
                     let xy = this.shapePlot.scaleFwd(primitive.coords);
-                    let node = dom.createSVG("text", {
-                        x: toStr(xy[0]), y: toStr(xy[1])
-                    }, [primitive.text]);
+                    let node = dom.label.toSVG(primitive.text);
+                    node.setAttribute("x", toStr(xy[0]));
+                    node.setAttribute("y", toStr(xy[1]));
                     dom.setAttributes(node, style);
                     dom.addEventListeners(node, events);
                     children.push(node);